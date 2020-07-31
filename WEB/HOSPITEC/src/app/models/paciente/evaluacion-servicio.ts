8cc20ddc6452b57b7f6865734b6f129acf6318ba
Josue Araya García <jdag98228@gmail.com>
2020-07-30 20:09:13 -0600

    Modelos
    Modelos listos, siguen servicios

--- WEB/HOSPITEC/src/app/models/paciente/evaluacion-servicio.ts
+++ WEB/HOSPITEC/src/app/models/paciente/evaluacion-servicio.ts
@@ -1,31 +1,18 @@
+
 export class EvaluacionServicio {
-}
+  idEvaluacion: number;
+  aseo: string;
+  trato: string;
+  puntualidad: string;
+  comentarios: string;
 
-/*import { parseHTML } from 'jquery';
-
-export class Contacts {
-  idContacto: number;
-  nombreContacto: string;
-  apellidos: string;
-  id: string;
-  edad: number;
-  nacionalidad: string;
-  direccion: string;
-  patologias: string;
-  email: string;
-  idPaciente: number;
-
-  constructor(id, name, lasName, contactId, age, nationality, address, pathologies, email, paciente) {
-    this.idContacto = id;
-    this.nombreContacto = name;
-    this.apellidos = lasName;
-    this.id = contactId;
-    this.edad = age;
-    this.nacionalidad = nationality;
-    this.direccion = address;
-    this.patologias = pathologies;
-    this.email = email;
-    this.idPaciente = paciente;
+  constructor(id, aseo, trato, puntualidad, comentarios) {
+    this.idEvaluacion = id;
+    this.aseo = aseo;
+    this.trato = trato;
+    this.puntualidad = puntualidad;
+    this.comentarios = comentarios;
   }
 
-}*/
+}
+
--- WEB/HOSPITEC/src/app/models/paciente/historial-clinico.ts
+++ WEB/HOSPITEC/src/app/models/paciente/historial-clinico.ts
@@ -1,2 +1,18 @@
 export class HistorialClinico {
+  idHistorial: number;
+  fecha: string;
+  procedimientoMedico: string;
+  tratamiento: string;
+  cedulaPaciente: string;
+  cedulaPersonal: string;
+
+  constructor(id, fecha, procedimiento, trat, paciente, personal) {
+    this.idHistorial = id;
+    this.fecha = fecha;
+    this.procedimientoMedico = procedimiento;
+    this.tratamiento = trat;
+    this.cedulaPaciente = paciente;
+    this.cedulaPersonal = personal;
+  }
+
 }
